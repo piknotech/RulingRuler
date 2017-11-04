7581b594702eeced1a84f972a83a7ae5b8de7571
Frederick Pietschmann <frederick.pietschmann@web.de>
2017-11-04 22:18:53 +0100

    Add project

---
+++ Ruling Ruler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ruling Ruler
+//
+//  Created by Frederick Pietschmann on 04.11.17.
+//  Copyright © 2017 Frederick Pietschmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
