ed133e22dd6038acf00a2f1b7338d5842344c02e
Frederick Pietschmann <19194800+fredpi@users.noreply.github.com>
2018-06-09 12:05:30 +0200

    Decelerate faster

--- Ruling Ruler/Sources/View Controllers/MainViewController.swift
+++ Ruling Ruler/Sources/View Controllers/MainViewController.swift
@@ -141,6 +141,7 @@ final class MainViewController: UIViewController {
         phantomScrollView.frame = view.bounds
         phantomScrollView.showsVerticalScrollIndicator = false
         phantomScrollView.showsHorizontalScrollIndicator = false
+        phantomScrollView.decelerationRate = UIScrollViewDecelerationRateFast
         phantomScrollView.contentSize = CGSize(
             width: view.bounds.width,
             height: DimensionInfo.pointsPerCentimeter * CGFloat(cmCount)
